90a5bc2b54531d7ff58edda48fe5f46f72bef807
Ayush Garg <ayush181000@gmail.com>
2024-01-29 01:40:51 +0530

    update brand

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,1 +1,13 @@
-export { default } from './logo';
+// export { default } from './logo';
+
+import React from 'react'
+
+export default function index() {
+    return (
+        <img src="./brand.svg" alt="logo" />
+    )
+};
+
+
+
+
