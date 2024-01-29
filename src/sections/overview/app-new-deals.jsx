74d8937edb39225261543b615b1919ea78fa7fae
Ayush Garg <ayush181000@gmail.com>
2024-01-30 01:48:40 +0530

    new deals

--- src/sections/overview/app-new-deals.jsx
+++ src/sections/overview/app-new-deals.jsx
@@ -4,36 +4,33 @@ export default function AppNewDeals({ ...other }) {
   return (
     <Box>
       <Card>
-        <CardHeader title="Top states" />
+        <CardHeader title="New deals" />
         <CardContent>
-          {states.map((state) => (
-            <div
-              key={state.state}
-              style={{
-                width: state.width,
-                // background: 'rgb(255,205,113)',
-                background:
-                  'linear-gradient(90deg, rgba(255,205,113,1) 50%, rgba(255,255,255,1) 100%)',
-                padding: '6px',
-                margin: '6px',
-                borderRadius: '4px',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-              }}
-            >
-              <Typography sx={{ fontSize: '14px', fontWeight: 600 }}>{state.state}</Typography>
-              <Typography
-                sx={{
-                  fontSize: '10px',
-                  fontWeight: 500,
+          <Box sx={{ display: 'flex', flexWrap: 'wrap ', gapX: '4px' }}>
+            {deals.map((deal) => (
+              <div
+                key={deal}
+                style={{
+                  background: '#FFF7E8',
+                  // background:
+                  //   'linear-gradient(90deg, rgba(255,205,113,1) 50%, rgba(255,255,255,1) 100%)',
+                  padding: '10px',
+                  margin: '6px',
+                  borderRadius: '16px',
+                  display: 'flex',
+                  flexDirection: 'row',
+                  justifyContent: 'flex-start',
+                  gap: '3px',
+                  alignItems: 'center',
                 }}
               >
-                {state.data}
-              </Typography>
-            </div>
-          ))}
+                {plusSvg}
+                <Typography sx={{ fontSize: '14px', fontWeight: 400, letterSpacing: '-0.3px' }}>
+                  {deal}
+                </Typography>
+              </div>
+            ))}
+          </Box>
         </CardContent>
       </Card>
     </Box>
@@ -42,25 +39,25 @@ export default function AppNewDeals({ ...other }) {
 
 AppNewDeals.propTypes = {};
 
-const states = [
-  {
-    state: 'NY',
-    data: '120K',
-    width: '272px',
-  },
-  {
-    state: 'MA',
-    data: '80K',
-    width: '229px',
-  },
-  {
-    state: 'NH',
-    data: '70K',
-    width: '207px',
-  },
-  {
-    state: 'OR',
-    data: '50K',
-    width: '150px',
-  },
+const deals = [
+  'Fruit2Go',
+  "Marshall's MKT",
+  'CCNT',
+  'Joana Mini-market',
+  'Little Brazil Vegan',
+  'Target',
+  'Organic Place',
+  "Morello's",
 ];
+
+const plusSvg = (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+    <path
+      d="M15 12.6116H9M12 9.61163V15.6116M12 3.61163C19.2 3.61163 21 5.41163 21 12.6116C21 19.8116 19.2 21.6116 12 21.6116C4.8 21.6116 3 19.8116 3 12.6116C3 5.41163 4.8 3.61163 12 3.61163Z"
+      stroke="#FFA500"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
--- src/sections/overview/view/app-view.jsx
+++ src/sections/overview/view/app-view.jsx
@@ -1,23 +1,18 @@
 /* eslint-disable import/no-extraneous-dependencies */
 // import { Line } from 'react-chartjs-2';
-import { faker } from '@faker-js/faker';
 
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Box, Card, Typography } from '@mui/material';
 
-import Iconify from 'src/components/iconify';
-
 // import { data } from '../chart';
 import { chats } from '../chats';
 import AppChat from '../app-chat';
-import AppTasks from '../app-tasks';
 import { customers } from '../customers';
+import AppNewDeals from '../app-new-deals';
 import AppTopStates from '../app-top-states';
 import AppSemicircle from '../app-semicircle';
-import AppOrderTimeline from '../app-order-timeline';
 import AppWidgetSummary from '../app-widget-summary';
-import AppTrafficBySite from '../app-traffic-by-site';
 import AppCustomersUpdate from '../app-customers-update';
 
 // ----------------------------------------------------------------------
@@ -290,66 +285,7 @@ export default function AppView() {
         </Grid>
 
         <Grid lg={6}>
-          <div />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppOrderTimeline
-            title="Order Timeline"
-            list={[...Array(5)].map((_, index) => ({
-              id: faker.string.uuid(),
-              title: [
-                '1983, orders, $4220',
-                '12 Invoices have been paid',
-                'Order #37745 from September',
-                'New order placed #XF-2356',
-                'New order placed #XF-2346',
-              ][index],
-              type: `order${index + 1}`,
-              time: faker.date.past(),
-            }))}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={4}>
-          <AppTrafficBySite
-            title="Traffic by Site"
-            list={[
-              {
-                name: 'FaceBook',
-                value: 323234,
-                icon: <Iconify icon="eva:facebook-fill" color="#1877F2" width={32} />,
-              },
-              {
-                name: 'Google',
-                value: 341212,
-                icon: <Iconify icon="eva:google-fill" color="#DF3E30" width={32} />,
-              },
-              {
-                name: 'Linkedin',
-                value: 411213,
-                icon: <Iconify icon="eva:linkedin-fill" color="#006097" width={32} />,
-              },
-              {
-                name: 'Twitter',
-                value: 443232,
-                icon: <Iconify icon="eva:twitter-fill" color="#1C9CEA" width={32} />,
-              },
-            ]}
-          />
-        </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AppTasks
-            title="Tasks"
-            list={[
-              { id: '1', name: 'Create FireStone Logo' },
-              { id: '2', name: 'Add SCSS and JS files if required' },
-              { id: '3', name: 'Stakeholder Meeting' },
-              { id: '4', name: 'Scoping & Estimations' },
-              { id: '5', name: 'Sprint Showcase' },
-            ]}
-          />
+          <AppNewDeals />
         </Grid>
       </Grid>
     </Container>
